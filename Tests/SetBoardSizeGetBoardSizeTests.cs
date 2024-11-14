bf07fda4964782f4666b83915fe8c8e8a325a298
AlfaPatens <aleksasdar@gmail.com>
2024-11-14 14:43:15 +0200

    testing prototype folder

---
+++ Tests/SetBoardSizeGetBoardSizeTests.cs
@@ -0,0 +1,66 @@
+﻿using Server;
+using Server.Strategy.ShipPlacement;
+using Server.AbstractFactory;
+using Microsoft.VisualStudio.CodeCoverage;
+
+namespace Tests
+{
+	public class SetBoardSizeGetBoardSizeTests
+	{
+		private PlayerOne _player;
+		[SetUp]
+		public void Setup()
+		{
+			_player = new PlayerOne("TestPlayer", new ManualShipPlacementStrategy());
+		}
+
+		[Test]
+		public void SetBoardSize_StandardSize_ShouldInitialize10x10Board()
+		{
+			// Act
+			_player.SetBoardSize(1);
+
+			// Assert
+			var board = _player.GetBoard();
+			Assert.That(board.GetLength(0), Is.EqualTo(10), "The board should have 10 rows for standard size.");
+			Assert.That(board.GetLength(1), Is.EqualTo(10), "The board should have 10 columns for standard size.");
+		}
+
+		[Test]
+		public void SetBoardSize_LargeSize_ShouldInitialize15x15Board()
+		{
+			// Act
+			_player.SetBoardSize(2);
+
+			// Assert
+			var board = _player.GetBoard();
+			Assert.That(board.GetLength(0), Is.EqualTo(15), "The board should have 15 rows for large size.");
+			Assert.That(board.GetLength(1), Is.EqualTo(15), "The board should have 15 columns for large size.");
+		}
+
+		[Test]
+		public void SetBoardSize_ExtraLargeSize_ShouldInitialize20x20Board()
+		{
+			// Act
+			_player.SetBoardSize(3);
+
+			// Assert
+			var board = _player.GetBoard();
+			Assert.That(board.GetLength(0), Is.EqualTo(20), "The board should have 20 rows for extra large size.");
+			Assert.That(board.GetLength(1), Is.EqualTo(20), "The board should have 20 columns for extra large size.");
+		}
+
+		[Test]
+		public void SetBoardSize_InvalidChoice_ShouldDefaultTo10x10Board()
+		{
+			// Act
+			_player.SetBoardSize(0);
+
+			// Assert
+			var board = _player.GetBoard();
+			Assert.That(board.GetLength(0), Is.EqualTo(10), "The board should default to 10 rows for invalid choice.");
+			Assert.That(board.GetLength(1), Is.EqualTo(10), "The board should default to 10 columns for invalid choice.");
+		}
+
+	}
+}
---
+++ Tests/ShipFactoryTests.cs
@@ -0,0 +1,76 @@
+﻿using Server.Prototype;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+	public class ShipFactoryTests
+	{
+		private ShipFactory _shipFactory;
+
+		[SetUp]
+		public void Setup()
+		{
+			_shipFactory = new ShipFactory();
+		}
+
+		[Test]
+		public void GetShip_ShouldReturnDestroyer_WhenShipTypeIsDestroyer()
+		{
+			// Act
+			var result = _shipFactory.GetShip("Destroyer");
+
+			// Assert
+			Assert.IsNotNull(result, "Expected a non-null result for Destroyer.");
+			Assert.IsInstanceOf<Destroyer>(result, "Expected the returned ship to be of type Destroyer.");
+		}
+
+		[Test]
+		public void GetShip_ShouldReturnSubmarine_WhenShipTypeIsSubmarine()
+		{
+			// Act
+			var result = _shipFactory.GetShip("Submarine");
+
+			// Assert
+			Assert.IsNotNull(result, "Expected a non-null result for Submarine.");
+			Assert.IsInstanceOf<Submarine>(result, "Expected the returned ship to be of type Submarine.");
+		}
+
+		[Test]
+		public void GetShip_ShouldReturnCruiser_WhenShipTypeIsCruiser()
+		{
+			// Act
+			var result = _shipFactory.GetShip("Cruiser");
+
+			// Assert
+			Assert.IsNotNull(result, "Expected a non-null result for Cruiser.");
+			Assert.IsInstanceOf<Cruiser>(result, "Expected the returned ship to be of type Cruiser.");
+		}
+
+		[Test]
+		public void GetShip_ShouldReturnBattleship_WhenShipTypeIsBattleship()
+		{
+			// Act
+			var result = _shipFactory.GetShip("Battleship");
+
+			// Assert
+			Assert.IsNotNull(result, "Expected a non-null result for Battleship.");
+			Assert.IsInstanceOf<Battleship>(result, "Expected the returned ship to be of type Battleship.");
+		}
+
+		[Test]
+		public void GetShip_ShouldReturnNull_WhenShipTypeIsInvalid()
+		{
+			// Act
+			var result = _shipFactory.GetShip("Frigate");
+
+			// Assert
+			Assert.IsNull(result, "Expected null for an unsupported ship type.");
+		}
+
+	}
+}
